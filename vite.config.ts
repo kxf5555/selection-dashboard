9e74cbb2f4b570b956583e89da567eba5f24535a
kxf5555 <kxf5555@gmail.com>
2025-05-31 04:33:53 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { '@': resolve(__dirname, './src') } }
-})
+  plugins:[react()],
+  resolve:{ alias:{ '@': resolve(__dirname, './src') } }
+});
